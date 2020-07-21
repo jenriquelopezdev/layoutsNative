04ef5061d4aa054a2a2f88ab9853ad62818b97bb
Enrique Lopez <elopez@digitalgeko.com>
2020-07-20 21:30:09 -0600

    ScrollView horizontal

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import {
     StyleSheet,
     View,
     Image,
+    Text,
+    ScrollView,
 } from 'react-native';
 
 const App: () => React$Node = () => {
@@ -23,6 +25,45 @@ const App: () => React$Node = () => {
                         source={require('./assets/img/bg.jpg')}
                     />
                 </View>
+                <View style={styles.container}>
+                    <Text style={styles.title}>
+                        Qué hacer en Paris
+                    </Text>
+                    <ScrollView
+                        horizontal
+                    >
+                        <View>
+                            <Image
+                                style={styles.city}
+                                source={require('./assets/img/actividad1.jpg')}
+                            />
+                        </View>
+                        <View>
+                            <Image
+                                style={styles.city}
+                                source={require('./assets/img/actividad2.jpg')}
+                            />
+                        </View>
+                        <View>
+                            <Image
+                                style={styles.city}
+                                source={require('./assets/img/actividad3.jpg')}
+                            />
+                        </View>
+                        <View>
+                            <Image
+                                style={styles.city}
+                                source={require('./assets/img/actividad4.jpg')}
+                            />
+                        </View>
+                        <View>
+                            <Image
+                                style={styles.city}
+                                source={require('./assets/img/actividad5.jpg')}
+                            />
+                        </View>
+                    </ScrollView>
+                </View>
             </View>
         </>
     );
@@ -32,6 +73,19 @@ const styles = StyleSheet.create({
     banner: {
         height: 250,
         flex: 1,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        marginVertical: 20,
+    },
+    container: {
+        marginHorizontal: 10,
+    },
+    city: {
+        width: 250,
+        height: 300,
+        marginRight: 10,
     },
 });
 
