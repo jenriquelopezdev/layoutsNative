8a6b43dd43b3af1d1c3426c3fc09e16539477ba5
Enrique Lopez <elopez@digitalgeko.com>
2020-07-20 21:22:24 -0600

    add image

--- App.js
+++ App.js
@@ -10,16 +10,29 @@ import React from 'react';
 import {
     StyleSheet,
     View,
+    Image,
 } from 'react-native';
 
 const App: () => React$Node = () => {
     return (
         <>
-
+            <View>
+                <View style={{flexDirection: 'row'}}>
+                    <Image
+                        style={styles.banner}
+                        source={require('./assets/img/bg.jpg')}
+                    />
+                </View>
+            </View>
         </>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    banner: {
+        height: 250,
+        flex: 1,
+    },
+});
 
 export default App;
