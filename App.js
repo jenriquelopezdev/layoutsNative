65d1e5231d1e28a76b2bdd45e79a971082ce8488
Enrique Lopez <elopez@digitalgeko.com>
2020-07-20 21:18:14 -0600

    assets

--- App.js
+++ App.js
@@ -15,52 +15,11 @@ import {
 const App: () => React$Node = () => {
     return (
         <>
-            <View style={styles.container}>
-                <View style={styles.box1}>
 
-                </View>
-                <View style={styles.box2}>
-
-                </View>
-                <View style={styles.box3}>
-
-                </View>
-                <View style={styles.box4}>
-
-                </View>
-            </View>
         </>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'cornflowerblue',
-        flex: 1,
-        flexDirection: 'column',
-        // justifyContent: 'center',// alinear vertical
-        alignItems: 'center',// alinear horizontal
-    },
-    box1: {
-        padding: 20,
-        backgroundColor: 'navy',
-
-    },
-    box2: {
-        padding: 20,
-        backgroundColor: 'yellow',
-
-    },
-    box3: {
-        padding: 20,
-        backgroundColor: 'green',
-
-    },
-    box4: {
-        padding: 20,
-        backgroundColor: 'teal',
-
-    },
-});
+const styles = StyleSheet.create({});
 
 export default App;
